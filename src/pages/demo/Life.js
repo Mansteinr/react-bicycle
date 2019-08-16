d1a2f568f977d6df26b59ae63f1aaed9a73d0395
manstein <manteinvip@sina.com>
2019-08-16 08:24:26 +0800

    修改主题色

--- src/pages/demo/Life.js
+++ src/pages/demo/Life.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import Child from './Child'
-<<<<<<< HEAD
 import { Button } from 'antd';
-=======
-import { Button, Radio, Icon } from 'antd';
->>>>>>> 7dc0d5137948b55e27a42caa539b4ed977fb8e11
 export default class Life extends React.Component{
   constructor(props) {
     super(props)
@@ -29,15 +25,10 @@ export default class Life extends React.Component{
       <button onClick={this.handleAdd}>点击一些</button>
       <button onClick={this.handClick.bind(this)}>点击一些</button>
       <p>{this.state.count}</p>
-<<<<<<< HEAD
-      <Button type="primary">Primary</Button>
-      <Child name={this.state.count}/>
-=======
       <Child name={this.state.count} />
       <Button type="primary">
           Primary
         </Button>
->>>>>>> 7dc0d5137948b55e27a42caa539b4ed977fb8e11
     </div>
   }
 }
