e1f86199ce2800b85e28078f07880fadc8e6bb16
manstein <manteinvip@sina.com>
2019-08-21 07:25:53 +0800

    头部组件完成 百度天气APi

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react'
 import { Row, Col } from 'antd'
 import './index.less'
+import Utils from '../../utils'
+import axios from '../../axios/index'
+
+// 百度天气API接口
+//http://api.map.baidu.com/telematics/v3/weather?location=beijing&output=json&ak=3p49MVra6urFRGOT9s8UBWr2
+let url = 'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent('南京')+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
 export default class Header extends Component{
+  // 必须先声明state才能setState
   state = {
     userName: 'world'
+  }
+  getWeatherAPIData () {
+    axios.jsonp({
+      url
+    }).then(res => {
+      let data = res.results[0].weather_data[0]
+      this.setState({
+        dayPictureUrl: data.dayPictureUrl,
+        weather: data.weather
+      })
+    })
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      let sysTime = Utils.formatterTime(+new Date())
+      return this.setState({
+        sysTime
+      })
+    }, 1000)
+    this.getWeatherAPIData()
   }
   render () {
     return (
@@ -17,8 +44,14 @@ export default class Header extends Component{
         <Row className="breadcrumb">
           <Col span={4} className="breadcrumb-title">首页</Col>
           <Col span={20} className="weather">
-            <span className="date">2019-02-23</span>
-            <span className="weather-detail">多云</span>
+            <span className="date">{this.state.sysTime} </span>
+            <span className="weather-img">
+              <img src={this.state.dayPictureUrl} alt="" />
+             
+            </span>
+            <span className="weather-detail">
+              { this.state.weather }
+            </span>
           </Col>
         </Row>
       </div>
---
+++ src/utils.js
@@ -0,0 +1,29 @@
+export default {
+  formatterTime (timestamp, format) {
+    //format:  "yyyy-m-d h:i:s.S","yyyy年mm月dd日 hh:ii:ss"  default: "yyyy-mm-dd hh:ii:ss"
+    var obj = parseInt(timestamp)
+    var date = new Date(obj)
+    var data = {
+      "m+": date.getMonth() + 1,                 //月   
+      "d+": date.getDate(),                    //日   
+      "h+": date.getHours(),                   //小时   
+      "i+": date.getMinutes(),                 //分   
+      "s+": date.getSeconds(),                 //秒   
+      "q+": Math.floor((date.getMonth() + 3) / 3), //季度   
+      "S": date.getMilliseconds()             //毫秒   
+    };
+    if (!format) {
+      format = "yyyy-mm-dd hh:ii:ss"
+    }
+    if (/(y+)/.test(format)) {
+      // date.getFullYear() + ""  转为字符串
+      format = format.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length))
+    }
+    for (var k in data) {
+      if (new RegExp("(" + k + ")").test(format)) {
+        format = format.replace(RegExp.$1, (RegExp.$1.length === 1) ? (data[k]) : (("00" + data[k]).substring(("" + data[k]).length)))
+      }
+    }
+    return format
+   }
+}
