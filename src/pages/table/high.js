b3191e7c46db1e2c8a995561339bc4b33b271740
manstein <manteinvip@sina.com>
2019-09-13 01:02:01 +0800

    左侧固定滚动   添加x要是各个td的宽度之和

--- src/pages/table/high.js
+++ src/pages/table/high.js
@@ -96,19 +96,121 @@ export default class High extends React.Component{
       }
     }, {
         title: '生日',
-        width: 80,
+        width: 120,
       key: 'birthday',
       dataIndex: 'birthday'
     }, {
       title: '地址',
         key: 'address',
+        width: 120,
+        dataIndex: 'address'
+    }, {
+        title: '早起时间',
+        key: 'time',
+        width: 120,
+        dataIndex: 'time'
+      }]
+    
+    
+    const columns2 = [{
+      title:'id',
+      key: 'id',
+      width: 80,
+      fixed: 'left',
+      dataIndex:'id'
+    }, {
+        title: '用户名',
+        fixed: 'left',
         width: 80,
+      key: 'userName',
+      dataIndex: 'userName'
+    }, {
+      title: '性别',
+        key: 'sex',
+        width: 80,
+      dataIndex: 'sex',
+      render(sex){
+        return sex ==1 ?'男':'女'
+      }
+    }, {
+      title: '状态',
+      key: 'state',
+      width: 80,
+      dataIndex: 'state',
+      render(state){
+        let config  = {
+          '1':'咸鱼一条',
+          '2':'风华浪子',
+          '3':'北大才子',
+          '4':'百度FE',
+          '5':'创业者'
+        }
+        return config[state];
+      }
+    }, {
+        title: '爱好',
+        width: 80,
+      key: 'interest',
+      dataIndex: 'interest',
+      render(abc) {
+        let config = {
+            '1': '游泳',
+            '2': '打篮球',
+            '3': '踢足球',
+            '4': '跑步',
+            '5': '爬山',
+            '6': '骑行',
+            '7': '桌球',
+            '8': '麦霸'
+        }
+        return config[abc];
+      }
+    }, {
+        title: '生日',
+        width: 120,
+      key: 'birthday',
+      dataIndex: 'birthday'
+    }, {
+      title: '生日',
+      width: 120,
+    key: 'birthday',
+    dataIndex: 'birthday'
+  }, {
+    title: '生日',
+    width: 120,
+  key: 'birthday',
+  dataIndex: 'birthday'
+}, {
+  title: '生日',
+  width: 120,
+key: 'birthday',
+dataIndex: 'birthday'
+}, {
+  title: '生日',
+  width: 120,
+key: 'birthday',
+dataIndex: 'birthday'
+}, {
+  title: '生日',
+  width: 120,
+key: 'birthday',
+dataIndex: 'birthday'
+}, {
+  title: '生日',
+  width: 120,
+key: 'birthday',
+dataIndex: 'birthday'
+}, {
+      title: '地址',
+        key: 'address',
+        width: 120,
       dataIndex: 'address'
     }, {
       title: '早起时间',
         key: 'time',
+        width: 120,
       dataIndex: 'time'
-    }]
+      }]
     return (
       <div>
         <Card title="头部固定">
@@ -123,9 +225,10 @@ export default class High extends React.Component{
         <Card title="左侧固定" style={{margin:'10px 0'}}>
           <Table
             bordered
-            columns={ columns }
+            columns={ columns2 }
             dataSource={ this.state.dataSource }
-            pagination={ true }
+            pagination={true}
+            scroll={{x:2700}}
           />
         </Card>
      </div>
